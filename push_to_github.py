15eecbc49acf68f5eac2f2cf471fcce2709edebc
Arash Mirshahi <arashmirshahi14@gmail.com>
2025-06-24 18:02:37 +0200

    Some generic changes

--- push_to_github.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Basic helper script to push changes to GitHub
-commit_msg = input("Enter commit message: ")
-os.system("git add .")
-os.system(f"git commit -m \"{commit_msg}\"")
-os.system("git push origin main")
